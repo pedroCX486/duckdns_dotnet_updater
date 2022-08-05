3b93b941f4c0aff0d9dc47b631bd0a8712c301fe
CX486 <pedrocx486@gmail.com>
2022-08-05 15:15:03 -0300

    Add new publish profiles, refactor code and upgrade .NET version

--- Program.cs
+++ Program.cs
@@ -1,32 +1,62 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Threading.Tasks;
 
 namespace DuckDNS_Updtr
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            string token = "YOUR_DUCK_DNS_TOKEN"; //Your DuckDNS token, present on your profile
-            string domain = "YOURDOMAIN"; //Your domain name, eg "myhome" instead of "myhome.duckdns.org"
+            #if (DEBUG)
+                Console.Write("Debug Mode - ");
+                args = new string[] { "duck-dns-token-goes-here", "subdomainname", "loop", "10" };
+            #endif
+
+            if (args is null || args.Length < 2)
+            {
+                Console.WriteLine("Arguments missing! Please inform required arguments for execution in order: \"token\" \"username\"");
+                Console.WriteLine("\nShould the application run indefinitely, after \"username\" inform the argument \"loop\" and");
+                Console.WriteLine("\nthe amount of time it should run, like every \"10\" minutes.");
+                Environment.Exit(0);
+            }
 
             Console.WriteLine("DuckDNS IP Updater");
 
-            updateDuckDNS(domain, token);
-            Environment.Exit(0);
+            await UpdateDuckDNS(args[0], args[1]);
+            
+            if(args[2] is not null)
+            {
+                while(true)
+                {
+                    Console.WriteLine("\nWaiting for the next cycle in " + args[3] + " minutes...\n");
+                    System.Threading.Thread.Sleep(int.Parse(args[3]) * 60 * 1000);
+                    await UpdateDuckDNS(args[0], args[1]);
+                }
+            } else
+            {
+                Environment.Exit(0);
+            }
         }
 
-        static void updateDuckDNS(string domain, string token)
+        static async System.Threading.Tasks.Task UpdateDuckDNS(string token, string domain)
         {
             try
             {
-                Uri url = new Uri("https://www.duckdns.org/update?domains=" + domain + "&token=" + token + "&ip=" + getIP());
+                var httpClient = new System.Net.Http.HttpClient(new System.Net.Http.HttpClientHandler() { UseDefaultCredentials = true });
+
+                var ip = await GetIP();
+                if(ip is null)
+                {
+                    throw new ArgumentException("Error: IP ca");
+                }
+
+                Uri url = new("https://www.duckdns.org/update?domains=" + domain + "&token=" + token + "&ip=" + ip);
 
                 Console.WriteLine("Sending request to DuckDNS...");
 
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                var response = await httpClient.GetAsync(url);
 
                 if (response.StatusCode.ToString().Contains("OK"))
                 {
@@ -34,54 +64,41 @@ namespace DuckDNS_Updtr
                 }
                 else
                 {
-                    Console.WriteLine("Error - StatusCode: " + response.StatusCode.ToString() + "\r\n" +
-                                      "Try running the application again or checking your internet connection.");
+                    Console.WriteLine("Error - StatusCode: " + response.StatusCode.ToString() 
+                        + "\nError updating your IP!  Try checking your internet connection.");
                 }
 
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
-                Console.WriteLine("Error! Try running the application again or checking your internet connection.");
-                Environment.Exit(1);
+                // Console.WriteLine(e);
+                Console.WriteLine("\nError updating your IP! Try checking your internet connection.");
             }
 
         }
 
-        static string getIP()
+        static async System.Threading.Tasks.Task<string> GetIP()
         {
-
-            string publicIP;
-
-            Console.WriteLine("\r\nFetching your public IP address...");
+            Console.WriteLine("\nFetching your public IP address...");
 
             try
             {
-                Uri url = new Uri("http://checkip.amazonaws.com");
-                
-                using (WebClient client = new WebClient())
-                {
-                    publicIP = client.DownloadString(url);
-                }
+                Uri url = new("http://checkip.amazonaws.com");
+
+                var httpClient = new System.Net.Http.HttpClient(new System.Net.Http.HttpClientHandler() { UseDefaultCredentials = true });
+                var response = await httpClient.GetAsync(url);
+                var publicIP = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine("Your public IP is: " + publicIP);
+                return publicIP;
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
-                publicIP = "Error. Couldn't fetch your public IP address. \r\n" +
-                           "Try running the application again or checking your internet connection.";
+                // Console.WriteLine(e);
+                Console.WriteLine("Error. Couldn't fetch your public IP address." +
+                    "\nTry checking your internet connection.");
+                return null;
             }
-
-            if (publicIP.Contains("Error"))
-            {
-                Console.WriteLine(publicIP);
-                Environment.Exit(1);
-            }
-            else
-            {
-                Console.WriteLine("Your public IP is: " + publicIP);
-            }
-
-            return publicIP;
         }
     }
 }
